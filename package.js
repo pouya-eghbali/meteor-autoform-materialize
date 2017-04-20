b1db8f8fa1de9162e18d3ce2b9cc04c5b0bc5a13
Wynand <groendrakie@gmail.com>
2017-04-20 16:42:50 +0200

    bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'mozfet:autoform-materialize',
   summary: 'Materialize theme for Autoform',
-  version: '2.0.5',
+  version: '2.0.6',
   git: 'https://github.com/mozfet/meteor-autoform-materialize.git'
 });
 
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
   api.use('ecmascript@0.7.2');
   api.use('momentjs:moment@2.10.6');
   api.use('aldeed:autoform@6.0.0');
-  api.use('mozfet:materialize-time-picker@0.1.0');
+  api.use('mozfet:materialize-time-picker@0.1.1');
   api.addFiles([
     'index.js'
   ], 'client');
