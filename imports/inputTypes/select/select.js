67e1103b692619f216562512b0afd8c3f1dec4e5
Wynand Vermeulen <>
2018-09-06 13:13:58 +0200

    #35

--- imports/inputTypes/select/select.js
+++ imports/inputTypes/select/select.js
@@ -1,24 +1,59 @@
-/*jshint esversion: 6 */
+import { Template } from 'meteor/templating'
+import { ReactiveVar } from 'meteor/reactive-var'
+import './select.html'
+import { attsToggleInvalidClass } from '../../utilities/attsToggleInvalidClass'
 
-import { Template } from 'meteor/templating';
-import './select.html';
-import { attsToggleInvalidClass } from '../../utilities/attsToggleInvalidClass';
-import { optionAtts } from '../../utilities/optionAtts';
-import { initializeSelect } from '../../utilities/initializeSelect';
+// Template.afSelect_materialize.onRendered(initializeSelect)
+Template.afSelect_materialize.onRendered(() => {
+  const instance = Template.instance()
+  instance.$('select').formSelect()
+})
 
 Template.afSelect_materialize.helpers({
   atts: attsToggleInvalidClass,
-  optionAtts: optionAtts,
-  firstValueSelected: function () {
-    const data = Template.currentData();
-    if (isEmptySelect(data.value) &&
-        (data.atts.firstOption || data.atts.placeholder)) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
-});
 
-Template.afSelect_materialize.onRendered(initializeSelect);
+  optionAtts(option) {
+    const data = Template.currentData()
+    console.log('select data', data)
+    console.log('option', option)
+    const atts = {
+      value: option.value,
+    }
+
+    // if option is empty first option
+    // if (option._id === 'AUTOFORM_EMPTY_FIRST_OPTION') {
+    //   if (data.atts.required !== undefined) {
+    //     atts.disabled = ''
+    //   }
+    // }
+
+    if (option.selected) {
+      atts.selected = ''
+    }
+    return atts
+  },
+
+  optionLabel(option) {
+
+    // if option is first empty option
+    if (option._id === 'AUTOFORM_EMPTY_FIRST_OPTION') {
+
+      // change label to placeholder if it exists
+      if (option.atts.placeholder) {
+        return option.atts.placeholder
+      }
+    }
+    return option.label
+  },
+
+  // firstValueSelected: function () {
+  //   const data = Template.currentData()
+  //   if (isEmptySelect(data.value) &&
+  //       (data.atts.firstOption || data.atts.placeholder)) {
+  //     return true
+  //   }
+  //   else {
+  //     return false
+  //   }
+  // }
+})
--- imports/utilities/optionAtts.js
+++ imports/utilities/optionAtts.js
@@ -1,23 +1,20 @@
-/*jshint esversion: 6 */
-
 export const optionAtts = function () {
   const option = this;
   const atts = {
     value: option.value,
-  };
+  }
 
   // if option is first empty option
   if (option._id === 'AUTOFORM_EMPTY_FIRST_OPTION') {
 
     // change label to placeholder if it exists
-    option.label = option.atts.placeholder?option.atts.placeholder:option.label;
+    option.label = option.atts.placeholder?option.atts.placeholder:option.label
 
-    // the first item cannot be selected
-    // atts.disabled = '';
+    atts.disabled = ''
   }
 
   if (option.selected) {
-    atts.selected = '';
+    atts.selected = ''
   }
   return atts;
-};
+}
