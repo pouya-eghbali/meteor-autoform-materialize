34d44c1832f334060d66327786f2676dd1902bc3
Pouya Eghbali <pouya.eghbali@yandex.com>
2020-02-20 13:37:44 +0100

    Fix select freeze on form data update

--- imports/inputTypes/select-multiple/select-multiple.js
+++ imports/inputTypes/select-multiple/select-multiple.js
@@ -1,214 +1,206 @@
 // impoers
-import { Template } from 'meteor/templating'
-import { ReactiveVar } from 'meteor/reactive-var'
-import './select-multiple.html'
-import { optionAtts } from '../../utilities/optionAtts'
-import { attsToggleInvalidClass } from '../../utilities/attsToggleInvalidClass'
-import { initializeSelect } from '../../utilities/initializeSelect'
-import './search.css'
+import { Template } from "meteor/templating";
+import { ReactiveVar } from "meteor/reactive-var";
+import "./select-multiple.html";
+import { optionAtts } from "../../utilities/optionAtts";
+import { attsToggleInvalidClass } from "../../utilities/attsToggleInvalidClass";
+import { initializeSelect } from "../../utilities/initializeSelect";
+import "./search.css";
 
 // worker functions
 function isEmptySelect(value) {
-  const valueIsEmptyArray = _.isArray(value) &&
-    (value.length === 1) && (_.isEmpty(_.first(value)))
+  const valueIsEmptyArray =
+    _.isArray(value) && value.length === 1 && _.isEmpty(_.first(value));
   if (_.isEmpty(value) || valueIsEmptyArray) {
-    return true
-  }
-  else {
-    return false
+    return true;
+  } else {
+    return false;
   }
 }
 
 function placeholder(data) {
   if (data.atts.firstOption) {
-    return data.atts.firstOption
-  }
-  else if (data.atts.placeholder) {
-    return data.atts.placeholder
-  }
-  else return undefined
+    return data.atts.firstOption;
+  } else if (data.atts.placeholder) {
+    return data.atts.placeholder;
+  } else return undefined;
 }
 
 function hasPlaceholder(data) {
-  return data.atts.firstOption || data.atts.placeholder ? true : false
+  return data.atts.firstOption || data.atts.placeholder ? true : false;
 }
 
 function createItems(data) {
-  const items = []
+  const items = [];
 
   // get selected values
-  let selectedValues = AutoForm.getFieldValue(data.atts.name, null, null, null, false)
+  let selectedValues = AutoForm.getFieldValue(
+    data.atts.name,
+    null,
+    null,
+    null,
+    false
+  );
 
   // normalise selected values (for multiple select)
   if (!_.isArray(selectedValues)) {
-    selectedValues = [selectedValues]
+    selectedValues = [selectedValues];
   }
   // console.log('selectedValues', selectedValues)
 
   // if there is a placeholder (or first option)
   if (hasPlaceholder(data)) {
-
     // define first option
     const firstOption = {
       atts: {
         htmlAttributes: {}
       },
       label: placeholder(data),
-      value: '',
+      value: "",
       disabled: true,
       selected: false,
-      _id: 'AUTOFORM_EMPTY_FIRST_OPTION'
-    }
+      _id: "AUTOFORM_EMPTY_FIRST_OPTION"
+    };
 
     // add first item
-    items.push(firstOption)
+    items.push(firstOption);
   }
 
   // for each item
   for (let item of data.items) {
-
     // if item is selected in old values
     if (_.contains(selectedValues, item.value)) {
-
       // push selected item
-      items.push(_.extend(item, { selected: true }))
+      items.push(_.extend(item, { selected: true }));
     }
 
     // else
     else {
-
       // push item
-      items.push(item)
+      items.push(item);
     }
   }
 
   // return items
   // console.log('created items', items)
-  return items
+  return items;
 }
 
 // on created
 Template.afSelectMultiple_materialize.onCreated(() => {
-  const instance = Template.instance()
+  const instance = Template.instance();
   // init items
-  instance.items = new ReactiveVar(createItems(instance.data))
-})
+  instance.items = new ReactiveVar(createItems(instance.data));
+});
 
 function throttle(fn, limit) {
-  let timeout
-  return function (...args) {
-    clearTimeout(timeout)
-    timeout = setTimeout(function () {
-      fn(...args)
-    }, limit)
-  }
+  let timeout;
+  return function(...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      fn(...args);
+    }, limit);
+  };
 }
 
 // on rendered
 Template.afSelectMultiple_materialize.onRendered(() => {
-  const instance = Template.instance()
+  const instance = Template.instance();
 
   // get select element, query
-  const selectQuery = instance.$('select')
-  const selectElement = selectQuery.get(0)
+  const selectQuery = instance.$("select");
+  const selectElement = selectQuery.get(0);
 
-  // react when template data changes
-  let oldItems
+  const materializeSelect = () => {
+    // init materialize select
+    if (!selectElement) return;
+    instance.selectInstance = M.FormSelect.init(selectElement);
+    const { data } = instance;
 
-  const onItemsChanged = data => {
-    // if items changed
-    if (!_.isEqual(oldItems, data.items)) {
-      // console.log('items changed', oldItems, data.items)
+    // select all options
 
-      // assign new items to old items
-      oldItems = _.clone(data.items)
-
-      // if select instance exists
-      if (instance.selectInstance) {
-
-        // destory previous instance
-        instance.selectInstance.destroy()
-      }
-
-      // create items
-      const itemData = { items: createItems(data) }
-
-      // remove all children of the select element
-      // console.log(selectQuery.get(0))
-      selectQuery.empty()
-
-      // render items template inside select element
-      Blaze.renderWithData(Template.afSelect_materialize_items, itemData, selectElement)
-
-      // init materialize select
-      instance.selectInstance = M.FormSelect.init(selectElement)
-
-      // select all options
-
-      if (data.atts.enableSelectAll) {
-        const selectAllText = data.atts.selectAllText || "Select all";
-        const selectNoneText = data.atts.selectNoneText || "Select none";
-        const ul = $(instance.selectInstance.dropdownOptions)
-        const selectAllEl = $(
-          `<li class="afSelectAllOption afNotAnActualSelectItem">
-            <span><label>${selectAllText}</label></span></li>`)
-        ul.prepend(selectAllEl)
-        selectAllEl.click(event => {
-          const selectAll = selectAllEl.find('label').text() == selectAllText
-          selectAllEl.find('label').text(selectAll ? selectNoneText : selectAllText)
-          ul.children('li').not('.afNotAnActualSelectItem').filter(function () {
-            return $(this).find('input').prop('checked') != selectAll;
-          }).click();
-        })
-      }
-
-      // search bar
-
-      if (data.atts.enableSearch) {
-        const placeholder = data.atts.searchPlaceholder || 'Search...'
-        const ul = $(instance.selectInstance.dropdownOptions)
-        const search = $(`<input placeholder="${placeholder}">`)
-        const searchBar = $(`<div class="afSelectSearchBar afNotAnActualSelectItem"></div>`)
-        const children = ul.children().not('.afNotAnActualSelectItem').toArray().map(child => {
-          return { el: child, content: child.innerText.toLowerCase() }
-        })
-        searchBar.append(search)
-        ul.prepend(searchBar)
-        search.on('keydown', event => event.stopImmediatePropagation())
-        search.on('keyup', event => {
-          const searchTerm = event.target.value.toLowerCase()
-          children.forEach(child => {
-            const { el, content } = child
-            if (content.includes(searchTerm)) {
-              el.style.display = 'list-item'
-            } else {
-              el.style.display = 'none'
-            }
+    if (data.atts.enableSelectAll) {
+      const selectAllText = data.atts.selectAllText || "Select all";
+      const selectNoneText = data.atts.selectNoneText || "Select none";
+      const ul = $(instance.selectInstance.dropdownOptions);
+      const selectAllEl = $(
+        `<li class="afSelectAllOption afNotAnActualSelectItem">
+            <span><label>${selectAllText}</label></span></li>`
+      );
+      ul.prepend(selectAllEl);
+      selectAllEl.click(event => {
+        const selectAll = selectAllEl.find("label").text() == selectAllText;
+        selectAllEl
+          .find("label")
+          .text(selectAll ? selectNoneText : selectAllText);
+        ul.children("li")
+          .not(".afNotAnActualSelectItem")
+          .filter(function() {
+            return (
+              $(this)
+                .find("input")
+                .prop("checked") != selectAll
+            );
           })
-        })
-      }
-
+          .click();
+      });
     }
-  }
 
-  const onItemsChangedThrottled = throttle(onItemsChanged, 100)
+    // search bar
 
-  instance.autorun(() => {
-    const data = Template.currentData()
-    // console.log('select template data', data)
-    onItemsChangedThrottled(data)
-  })
-})
+    if (data.atts.enableSearch) {
+      const placeholder = data.atts.searchPlaceholder || "Search...";
+      const ul = $(instance.selectInstance.dropdownOptions);
+      const search = $(`<input placeholder="${placeholder}">`);
+      const searchBar = $(
+        `<div class="afSelectSearchBar afNotAnActualSelectItem"></div>`
+      );
+      const children = ul
+        .children()
+        .not(".afNotAnActualSelectItem")
+        .toArray()
+        .map(child => {
+          return {
+            el: child,
+            content: child.innerText.toLowerCase()
+          };
+        });
+      searchBar.append(search);
+      ul.prepend(searchBar);
+      search.on("keydown", event => event.stopImmediatePropagation());
+      search.on("keyup", event => {
+        const searchTerm = event.target.value.toLowerCase();
+        children.forEach(child => {
+          const { el, content } = child;
+          if (content.includes(searchTerm)) {
+            el.style.display = "list-item";
+          } else {
+            el.style.display = "none";
+          }
+        });
+      });
+    }
+  };
+
+  instance
+    .$("select")
+    .change(materializeSelect)
+    .change()
+    .on("DOMSubtreeModified", materializeSelect);
+});
 
 // helpers
 Template.afSelectMultiple_materialize.helpers({
   atts: attsToggleInvalidClass,
   optionAtts(option) {
-    const atts = { value: option.value }
-    if (option.selected) { atts.selected = true }
-    if (option.disabled) { atts.disabled = true }
+    const atts = { value: option.value };
+    if (option.selected) {
+      atts.selected = true;
+    }
+    if (option.disabled) {
+      atts.disabled = true;
+    }
     // console.log(`optionAtts for option ${option.label}`, atts)
-    return atts
-  },
-
-})
+    return atts;
+  }
+});
--- imports/inputTypes/select/select.js
+++ imports/inputTypes/select/select.js
@@ -20,130 +20,104 @@ function throttle(fn, limit) {
 Template.afSelect_materialize.onRendered(() => {
   const instance = Template.instance();
 
-  // get select element, query
-  const selectQuery = instance.$("select");
-  const selectElement = selectQuery.get(0);
-
-  instance.$("select").change(function() {
+  const materializeSelect = () => {
+    // get select element, query
+    const selectQuery = instance.$("select");
+    const selectElement = selectQuery.get(0);
+    if (!selectElement) return;
     instance.selectInstance = M.FormSelect.init(selectElement);
-  });
+    const { data } = instance;
 
-  // react when template data changes
-  let oldItems;
-  const onItemsChanged = data => {
-    // if items changed
-    if (!_.isEqual(oldItems, data.items)) {
-      // console.log('items changed', oldItems, data.items)
-
-      // assign new items to old items
-      oldItems = _.clone(data.items);
-
-      // if select instance exists
-      let selectedValues;
-      if (instance.selectInstance) {
-        // get selected values
-        selectedValues = AutoForm.getFieldValue(
-          data.atts.name,
-          null,
-          null,
-          null,
-          false
-        );
-
-        // normalise selected values (for multiple select)
-        if (!_.isArray(selectedValues)) {
-          selectedValues = [selectedValues];
-        }
-        // console.log('old selectedValues', selectedValues)
-
-        // select previous selected values in new items
-        data.items = _.map(data.items, item => {
-          return _.contains(selectedValues, item.value)
-            ? _.extend(item, { selected: true })
-            : item;
-        });
-        // console.log('data.items', data.items)
-
-        // destory previous instance of materialize select
-        instance.selectInstance.destroy();
+    function ensureSearchBar() {
+      const { dropdownOptions } = instance.selectInstance;
+      if (!$(dropdownOptions).find(".afSelectSearchBar").length) {
+        maybeMakeSearchBar();
       }
-
-      // remove all children of the select element
-      selectQuery.empty();
-
-      // render items template inside select element
-      Blaze.renderWithData(
-        Template.afSelect_materialize_items,
-        data,
-        selectElement
-      );
-
-      // init materialize select
-      instance.selectInstance = M.FormSelect.init(selectElement);
-
-      // search bar
-
-      function ensureSearchBar() {
-        if (
-          !$(instance.selectInstance.dropdownOptions).find(".afSelectSearchBar")
-            .length
-        ) {
-          maybeMakeSearchBar();
-        }
-      }
-
-      function maybeMakeSearchBar() {
-        if (data.atts.enableSearch) {
-          const placeholder = data.atts.searchPlaceholder || "Search...";
-          const ul = $(instance.selectInstance.dropdownOptions);
-          const search = $(`<input placeholder="${placeholder}">`);
-          const searchBar = $(`<div class="afSelectSearchBar"></div>`);
-          const children = ul
-            .children()
-            .toArray()
-            .map(child => {
-              return { el: child, content: child.innerText.toLowerCase() };
-            });
-          searchBar.append(search);
-          ul.prepend(searchBar);
-
-          if (
-            instance.selectInstance.dropdown &&
-            instance.selectInstance.dropdown.options
-          ) {
-            instance.selectInstance.dropdown.options.closeOnClick = false;
-            search.on("keydown", event => event.stopImmediatePropagation());
-            instance.selectInstance.dropdown.options.onCloseEnd = ensureSearchBar;
-          }
-
-          search.on("keyup", event => {
-            const searchTerm = event.target.value.toLowerCase();
-            children.forEach(child => {
-              const { el, content } = child;
-              if (content.includes(searchTerm)) {
-                el.style.display = "list-item";
-              } else {
-                el.style.display = "none";
-              }
-            });
-          });
-        }
-      }
-
-      maybeMakeSearchBar();
     }
+
+    function maybeMakeSearchBar() {
+      if (data.atts.enableSearch) {
+        const placeholder = data.atts.searchPlaceholder || "Search...";
+        const ul = $(instance.selectInstance.dropdownOptions);
+        const search = $(`<input placeholder="${placeholder}">`);
+        const searchBar = $(`<div class="afSelectSearchBar"></div>`);
+        const children = ul
+          .children()
+          .toArray()
+          .map(child => {
+            return {
+              el: child,
+              content: child.innerText.toLowerCase()
+            };
+          });
+        searchBar.append(search);
+        ul.prepend(searchBar);
+
+        if (
+          instance.selectInstance.dropdown &&
+          instance.selectInstance.dropdown.options
+        ) {
+          instance.selectInstance.dropdown.options.closeOnClick = false;
+          search.on("keydown", event => event.stopImmediatePropagation());
+          instance.selectInstance.dropdown.options.onCloseEnd = ensureSearchBar;
+        }
+
+        search.on("keyup", event => {
+          const searchTerm = event.target.value.toLowerCase();
+          children.forEach(child => {
+            const { el, content } = child;
+            if (content.includes(searchTerm)) {
+              el.style.display = "list-item";
+            } else {
+              el.style.display = "none";
+            }
+          });
+        });
+      }
+    }
+
+    maybeMakeSearchBar();
   };
-  const onItemsChangedThrottled = throttle(onItemsChanged, 100);
-  instance.autorun(() => {
-    const data = Template.currentData();
-    // console.log('select template data', data)
-    onItemsChangedThrottled(data);
-  });
+
+  instance
+    .$("select")
+    .change(materializeSelect)
+    .change()
+    .on("DOMSubtreeModified", materializeSelect);
 });
 
 // helpers
 Template.afSelect_materialize.helpers({
-  atts: attsToggleInvalidClass
+  atts: attsToggleInvalidClass,
+
+  // get DOM attributes for an option
+  optionAtts(option) {
+    const atts = { value: option.value };
+    if (option.selected) {
+      atts.selected = "";
+    }
+    if (option.disabled) {
+      atts.disabled = "";
+    }
+    if (option.atts && option.atts.htmlAttributes) {
+      _.extend(atts, option.atts.htmlAttributes);
+    }
+    // console.log(`optionAtts for option ${option.label}`, atts)
+    return atts;
+  },
+
+  // get label for an option
+  optionLabel(option) {
+    if (option._id === "AUTOFORM_EMPTY_FIRST_OPTION") {
+      if (option.atts.placeholder) {
+        return option.atts.placeholder;
+      }
+      if (option.atts.firstOption) {
+        return option.atts.firstOption;
+      }
+    }
+    return option.label;
+  }
 });
 
 // on destroyed
