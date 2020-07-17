1230d964e8b64d89ad22c8282ba2a71e6d9077c9
Pouya <pouya.eghbali@yandex.com>
2020-07-17 16:30:46 +0200

    Check afFieldInput.required to add a required asterisk

--- imports/inputTypes/label/label.js
+++ imports/inputTypes/label/label.js
@@ -1,12 +1,15 @@
 /*jshint esversion: 6 */
 
-import { Template } from 'meteor/templating';
-import './label.html';
+import { Template } from "meteor/templating";
+import "./label.html";
 
 Template.afLabel_materialize.helpers({
-  atts: function() {
+  atts: function () {
     const instance = Template.instance();
     const labelAtts = instance.afFieldLabelAtts;
     return labelAtts;
-  }
+  },
+  isRequired() {
+    return this.required || this.afFieldInputAtts.required;
+  },
 });
